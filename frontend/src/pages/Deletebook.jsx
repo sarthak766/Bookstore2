10c2b991dcc94b2f25179b71b243d8af49ef67ff
sarthak766 <98082282+sarthak766@users.noreply.github.com>
2024-02-25 13:27:44 +0530

    Update Deletebook.jsx

--- frontend/src/pages/Deletebook.jsx
+++ frontend/src/pages/Deletebook.jsx
@@ -28,20 +28,26 @@ const Deletebook = () => {
   };
 
   return (
-    <div className="p-4" style={{ backgroundImage: "url('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/The_House_of_Leaves_-_Burning_4.jpg/1200px-The_House_of_Leaves_-_Burning_4.jpg')", backgroundSize: 'cover', backgroundPosition: 'center',height:'100vh',width:'100vw' }}>
+       <div className="p-4 flex flex-col items-center justify-center" style={{ 
+      backgroundImage: "url('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/The_House_of_Leaves_-_Burning_4.jpg/1200px-The_House_of_Leaves_-_Burning_4.jpg')", 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center',
+      height:'100vh',
+      width:'100vw' 
+  }}>
       <Backbutton />
       <h1 className="text-3xl my-4 text-white">Delete book</h1>
       {loading ? <Spinner /> : ''}
-      <div className="flex flex-col items-center border-2 bg-white border-sky-400 rounded-xl w-[600px] p-8 mx-auto md:mt-[200px] sm:mt-[150px] mt-150px">
-        <h3 className="text-2xl">Are You sure you want to delete Project?</h3>
-        <button
-          className="p-4 bg-red-700 text-white m-8 w-full"
-          onClick={handleDeleteBook}
-        >
-          Yes Delete it.
-        </button>
+      <div className="flex flex-col items-center border-2 bg-white border-sky-400 rounded-xl p-8 w-full max-w-sm mt-8">
+          <h3 className="text-2xl mb-8">Are You sure you want to delete Project?</h3>
+          <button
+              className="p-4 bg-red-700 text-white w-full"
+              onClick={handleDeleteBook}
+          >
+              Yes, Delete it.
+          </button>
       </div>
-    </div>
+  </div>
   );
 };
 
