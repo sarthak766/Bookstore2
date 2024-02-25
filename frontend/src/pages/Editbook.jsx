f16f4f295d7dbf39d3ec28e8f65874af0f327717
sarthak766 <98082282+sarthak766@users.noreply.github.com>
2024-02-25 16:04:43 +0530

    Update Editbook.jsx

--- frontend/src/pages/Editbook.jsx
+++ frontend/src/pages/Editbook.jsx
@@ -53,36 +53,35 @@ const Editbook = () => {
         <h1 className='text-3xl text-white my-4'>Edit Book</h1>
         {loading && <Spinner />}
         <div className='flex flex-col border-2 bg-white border-sky-400 rounded-xl p-4 mx-auto sm:w-full sm:max-w-sm'>
-  <div className='my-4'>
-    <label className='text-4xl mr-4 text-gray-500'>Title</label>
-    <input
-      type='text'
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      className='border-2 border-gray-500 px-4 py-2 w-full'
-    />
-  </div>
-  <div className='my-4'>
-    <label className='text-4xl mr-4 text-gray-500'>Author</label>
-    <input
-      type='text'
-      value={author}
-      onChange={(e) => setAuthor(e.target.value)}
-      className='border-2 border-gray-500 px-4 py-2 w-full'
-    />
-  </div>
-  <div className='my-4'>
-    <label className='text-4xl mr-4 text-gray-500'>Publish Year</label>
-    <input
-      type='text'
-      value={publishYear}
-      onChange={(e) => setPublishYear(e.target.value)}
-      className='border-2 border-gray-500 px-4 py-2 w-full'
-    />
-  </div>
-  <button className='rounded-md bg-blue-800 hover:bg-red-500 w-full h-10 text-white hover:text-black' onClick={handleEditBook}>Save</button>
-</div>
-
+          <div className='my-4'>
+            <label className='text-4xl mr-4 text-gray-500'>Title</label>
+            <input
+              type='text'
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <div className='my-4'>
+            <label className='text-4xl mr-4 text-gray-500'>Author</label>
+            <input
+              type='text'
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <div className='my-4'>
+            <label className='text-4xl mr-4 text-gray-500'>Publish Year</label>
+            <input
+              type='text'
+              value={publishYear}
+              onChange={(e) => setPublishYear(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full'
+            />
+          </div>
+          <button className='rounded-md bg-blue-800 hover:bg-red-500 w-full h-10 text-white hover:text-black' onClick={handleEditBook}>Save</button>
+        </div>
       </div>
     </div>
   );
