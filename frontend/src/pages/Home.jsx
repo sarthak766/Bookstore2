85cd734ec783180a7881e3fb40b36ed378c7f04c
sarthak766 <98082282+sarthak766@users.noreply.github.com>
2024-02-25 12:54:55 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineAddBox } from "react-icons/md";
 import Spinner from "../components/Spinner";
 import Bookscard from "../components/home/Bookscard";
 import Bookstable from "../components/home/Bookstable";
+
 const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [showtype,setShowType]=useState('table');
+  const [showType, setShowType] = useState('table');
+
   useEffect(() => {
     setLoading(true);
     axios
-      .get("https://bookstore2-one.vercel.app/books")
+      .get("http://localhost:4000/books")
       .then((res) => {
         setBooks(res.data.data);
         setLoading(false);
@@ -26,21 +26,17 @@ const Home = () => {
   },[]);
 
   return (
-    <div className="p-4" style={{ backgroundImage: "url('https://img.freepik.com/premium-photo/books-library-generative-ai_220873-19768.jpg?w=1060')",  backgroundSize: 'cover', backgroundPosition: 'center',height:'100%',width:'100vw',backgroundAttachment: 'fixed' }}>
+    <div className="p-4 flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-fixed" style={{ backgroundImage: "url('https://img.freepik.com/premium-photo/books-library-generative-ai_220873-19768.jpg?w=1060')" }}>
       <div className="flex justify-center items-center gap-x-4">
-           <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-            onClick={()=>setShowType('table')}
-           >
-            Table
-           </button>
-           <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
-            onClick={()=>setShowType('card')}
-           >
-            Card
-           </button>
+        <button className={`bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg ${showType === 'table' ? 'bg-sky-600' : ''}`} onClick={() => setShowType('table')}>
+          Table
+        </button>
+        <button className={`bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg ${showType === 'card' ? 'bg-sky-600' : ''}`} onClick={() => setShowType('card')}>
+          Card
+        </button>
       </div>
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl text-white my-8">Books-List</h1>
+      <div className="flex justify-between items-center w-full">
+        <h1 className="text-3xl my-8 text-white">Books-List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-white text-4xl" />
         </Link>
@@ -48,7 +44,7 @@ const Home = () => {
       {loading ? (
         <Spinner />
       ) : (
-          showtype === 'table' ?(<Bookstable books={books}/>):(<Bookscard books={books}/>)       
+        showType === 'table' ? (<Bookstable books={books}/>) : (<Bookscard books={books}/>)
       )}
     </div>
   );
