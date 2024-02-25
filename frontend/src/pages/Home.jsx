cbfe3db7731021a0d22f86ccedc2139fbe97249e
sarthak766 <98082282+sarthak766@users.noreply.github.com>
2024-02-25 12:44:47 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
   },[]);
 
   return (
-    <div className="p-4" style={{ backgroundImage: "url('https://img.freepik.com/premium-photo/books-library-generative-ai_220873-19768.jpg?w=1060')", backgroundSize: 'cover', backgroundPosition: 'center',height:'100vh',width:'!00vw' }}>
+    <div className="p-4" style={{ backgroundImage: "url('https://img.freepik.com/premium-photo/books-library-generative-ai_220873-19768.jpg?w=1060')",  backgroundSize: 'cover', backgroundPosition: 'center',height:'100%',width:'100vw',backgroundAttachment: 'fixed' }}>
       <div className="flex justify-center items-center gap-x-4">
            <button className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
             onClick={()=>setShowType('table')}
@@ -40,7 +40,7 @@ const Home = () => {
            </button>
       </div>
       <div className="flex justify-between items-center">
-        <h1 className="text-3xl my-8">Books-List</h1>
+        <h1 className="text-3xl text-white my-8">Books-List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-white text-4xl" />
         </Link>
